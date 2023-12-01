0286c59f012f6a6f6eefeb1c6d600ff2a430fa02
Gustavo Daniel <d2021020134@unifei.edu.br>
2023-12-01 16:34:21 -0300

    Pagina de erro

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,11 @@ function Home() {
       <br />
       <h1>Essa é a nossa Home</h1> <br />
       <Link to="/sobre">Sobre</Link> <br /> <br />
-      <Link to="/contato">Contato</Link>
+      <Link to="/contato">Contato</Link> 
+
+      <hr />
+      <br />
+      <Link to='/produto/123'>Acessar produto 123</Link>
     </div>
   );
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import Sobre from './pages/Sobre';
 import Contato from './pages/Contato';
 import Header from './components/Header';
 import Erro from './pages/Erro';
+import Produto from './pages/Produto';
 function RoutesApp(){
     return(
         
@@ -13,6 +14,7 @@ function RoutesApp(){
         <Route path='/home' element={ <Home/> }></Route>
         <Route path = '/sobre' element={<Sobre/>}></Route>
         <Route path = '/contato' element={<Contato/>}></Route>
+        <Route path = '/produto/:id' element={<Produto/>}></Route>
 
         <Route path='*' element={<Erro/>}/>
        </Routes>
